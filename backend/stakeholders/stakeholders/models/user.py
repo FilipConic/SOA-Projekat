56744dfe405043c7003fe42a9e07204df2a9e4f1
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-04-11 19:11:04 +0200

    fix: Made some changes to user class, removed unnecessary fields.

--- backend/stakeholders/stakeholders/models/user.py
+++ backend/stakeholders/stakeholders/models/user.py
@@ -1,16 +1,37 @@
-from django.contrib.auth.models import AbstractUser
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
     class Role(models.TextChoices):
-        ADMIN = 'ADMIN', 'Admin',
-        Tourist = 'TOURIST', 'Tourist',
-        Guide = 'GUIDE', 'Guide',
-    role = models.CharField(max_length=20, choices=Role.choices, default=Role.Tourist)
+        ADMIN = 'ADMIN', 'Admin'
+        TOURIST = 'TOURIST', 'Tourist'
+        GUIDE = 'GUIDE', 'Guide'
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
-
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.TOURIST)
     is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     def __str__(self):
         return self.username
+
+    class Meta:
+        db_table = 'users'
